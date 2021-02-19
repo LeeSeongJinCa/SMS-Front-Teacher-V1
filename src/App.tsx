ffe113d2fee920c0ea24cafbbb342b119ba6cfbf
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-02-19 12:05:27 +0900

    [MODIFY] <App> App 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,32 @@
 import React, { FC } from "react";
 import { Switch, Router, Route, Redirect } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-import "./lib/confirm/confirm.css";
+import { ToastContainer } from "react-toastify";
 
+import "./lib/confirm/confirm.css";
 import { GlobalStyle, GlobalContainer, GlobalBody } from "./GlobalStyle";
 import { PageNotFound, Navigation } from "./components";
-import {
-  LoginContainer,
-  HeaderContainer,
-  PasswordChangeContainer,
-  ParentContainer
-} from "./containers";
-import {
-  CirclesRouter,
-  NoticeRouter,
-  OutingRouter,
-  MainRouter,
-  AdminRouter,
-  ManagementRouter
-} from "./routers";
+import { HeaderContainer } from "./containers";
+import { AdminRouter } from "./routers";
 import { history } from "./modules/store";
-import { ToastContainer } from "react-toastify";
 
 const App: FC<{}> = () => {
   return (
-    <>
-      {location.pathname.includes("parent") ? (
-        <Router history={history}>
-          <ToastContainer autoClose={2000} />
-          <GlobalStyle />
+    <GlobalContainer>
+      <GlobalStyle />
+      <Router history={history}>
+        <ToastContainer autoClose={2000} />
+        <Navigation />
+        <GlobalBody>
+          <HeaderContainer />
           <Switch>
-            <Route path="/parent/:confirmUuid" component={ParentContainer} />
+            <Route path="/*" component={AdminRouter} />
+            <Redirect path="/" to="/home" />
+            <Route path="*" component={PageNotFound} />
           </Switch>
-        </Router>
-      ) : (
-        <GlobalContainer>
-          <GlobalStyle />
-          <Router history={history}>
-            <ToastContainer autoClose={2000} />
-            <Navigation />
-            <GlobalBody>
-              <HeaderContainer />
-              <Switch>
-                <Route path="/pw-change" component={PasswordChangeContainer} />
-                <Route path="/login" component={LoginContainer} />
-                <Route path="/home" component={MainRouter} />
-                <Route path="/notice" component={NoticeRouter} />
-                <Route path="/circles" component={CirclesRouter} />
-                <Route path="/outing" component={OutingRouter} />
-                <Route path="/admin" component={AdminRouter} />
-                <Route path="/management" component={ManagementRouter} />
-                <Redirect path="/" to="/home" />
-                <Route path="*" component={PageNotFound} />
-              </Switch>
-            </GlobalBody>
-          </Router>
-        </GlobalContainer>
-      )}
-    </>
+        </GlobalBody>
+      </Router>
+    </GlobalContainer>
   );
 };
 
