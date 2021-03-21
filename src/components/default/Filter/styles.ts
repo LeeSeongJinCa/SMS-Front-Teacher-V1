4d2ae01e4580310db89aecf10f071131706c3998
YeongG <yeonggil223@gmail.com>
2021-03-21 23:25:05 +0900

    [MODIFY] 각종 스타일 변경

--- src/components/default/Filter/styles.ts
+++ src/components/default/Filter/styles.ts
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components";
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 150px;
+  width: 200px;
 `;
 
 export const FilterWrap = styled.div`
@@ -25,9 +25,28 @@ export const Triangle = styled.div`
   margin-right: 5px;
 `;
 
-export const ResetBtn = styled.button`
+export const ToggleBtn = styled.button<{ isActive?: boolean }>`
+  width: 50px;
+  border: none;
+  background-color: transparent;
+  transition: 250ms all;
+  text-align: center;
+  cursor: pointer;
+  & + & {
+    border-left: 0.5px solid #dddddd;
+  }
+
+  ${props =>
+    props.isActive &&
+    css`
+      background-color: #23b2ad;
+    `}
+`;
+
+export const ResetBtn = styled.div`
   border: 0.5px solid #dddddd;
   background: transparent;
+  display: flex;
 `;
 
 export const SettingType = styled.button<{ active: boolean }>`
