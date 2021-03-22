46a73024dd9adb78ffda9795ca01874774385c13
YeongG <yeonggil223@gmail.com>
2021-03-22 23:13:56 +0900

    [MODIFY] navigation 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -37,6 +37,8 @@ export { default as NavIconUnauthorizedBlack } from "./navigation/NavIcon-unAuth
 export { default as NavIconUnauthorizedMint } from "./navigation/NavIcon-unAuthorized-mint.svg";
 export { default as NavIconExitBlack } from "./navigation/NavIcon-exit-black.svg";
 export { default as NavIconTrashCanYellow } from "./navigation/NavIcon-trashCan-yellow.svg";
+export { default as NavIconWaitBlack } from "./navigation/NavIcon-wait-black.svg";
+export { default as NavIconWaitMint } from "./navigation/NavIcon-wait-mint.svg";
 export { default as IconWhite } from "./navigation/Icon-white.svg";
 export { default as IconBlack } from "./navigation/Icon-Black.svg";
 export { default as CloseMenu } from "./navigation/CloseMenu.svg";
