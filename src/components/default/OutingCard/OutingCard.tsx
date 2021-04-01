b3d03c0aaa6967dce4784b8e386708df9a83173d
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-01 09:17:05 +0900

    [MODIFY] <components/default/OutingCard> Svg 태그 분할

--- src/components/default/OutingCard/OutingCard.tsx
+++ src/components/default/OutingCard/OutingCard.tsx
@@ -40,40 +40,7 @@ const OutingCard: FC<Props> = ({
         <S.RightHeader>
           {outing_situation === "EMERGENCY" ? (
             <S.EmergencyIconWrap name="긴급 외출">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="13"
-                height="13"
-                viewBox="0 0 13 13"
-              >
-                <g
-                  id="그룹_450"
-                  data-name="그룹 450"
-                  transform="translate(0 -0.096)"
-                >
-                  <path
-                    id="다각형_10"
-                    data-name="다각형 10"
-                    d="M6.5,0,13,11H0Z"
-                    transform="translate(0 0.096)"
-                    fill="#f55"
-                  />
-                  <text
-                    id="_"
-                    data-name="!"
-                    transform="translate(8 10.096)"
-                    fill="#fff"
-                    fontSize="9"
-                    fontFamily="NotoSansCJKkr-Medium, Noto Sans CJK KR"
-                    fontWeight="500"
-                    letterSpacing="-0.02em"
-                  >
-                    <tspan x="-3.087" y="0">
-                      !
-                    </tspan>
-                  </text>
-                </g>
-              </svg>
+              <EmergencySvg />
               <S.Arrow />
             </S.EmergencyIconWrap>
           ) : (
@@ -81,23 +48,7 @@ const OutingCard: FC<Props> = ({
           )}
           {arrival_time && arrival_time > end_time ? (
             <S.LateIconWrap name="지각">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="44"
-                height="44"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="#2c3e50"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <circle cx="12" cy="13" r="7" />
-                <polyline points="12 10 12 13 14 13" />
-                <line x1="7" y1="4" x2="4.25" y2="6" />
-                <line x1="17" y1="4" x2="19.75" y2="6" />
-              </svg>
+              <LateSvg />
               <S.Arrow />
             </S.LateIconWrap>
           ) : (
@@ -113,13 +64,70 @@ const OutingCard: FC<Props> = ({
           <S.FlexBetween>
             <S.Bar>-</S.Bar>
             <div>
-              <div>{startTime}</div>
-              <div>{endTime}</div>
+              <div>오후 {startTime}</div>
+              <div>오후 {endTime}</div>
             </div>
           </S.FlexBetween>
         </S.FlexBetween>
       </S.Body>
     </S.Container>
+  );
+};
+
+const EmergencySvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="13"
+      height="13"
+      viewBox="0 0 13 13"
+    >
+      <g id="그룹_450" data-name="그룹 450" transform="translate(0 -0.096)">
+        <path
+          id="다각형_10"
+          data-name="다각형 10"
+          d="M6.5,0,13,11H0Z"
+          transform="translate(0 0.096)"
+          fill="#f55"
+        />
+        <text
+          id="_"
+          data-name="!"
+          transform="translate(8 10.096)"
+          fill="#fff"
+          fontSize="9"
+          fontFamily="NotoSansCJKkr-Medium, Noto Sans CJK KR"
+          fontWeight="500"
+          letterSpacing="-0.02em"
+        >
+          <tspan x="-3.087" y="0">
+            !
+          </tspan>
+        </text>
+      </g>
+    </svg>
+  );
+};
+
+const LateSvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="44"
+      height="44"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="#2c3e50"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <circle cx="12" cy="13" r="7" />
+      <polyline points="12 10 12 13 14 13" />
+      <line x1="7" y1="4" x2="4.25" y2="6" />
+      <line x1="17" y1="4" x2="19.75" y2="6" />
+    </svg>
   );
 };
 
