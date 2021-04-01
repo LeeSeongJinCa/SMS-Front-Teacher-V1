1c140ef046f600cd4ed565ab0140f6b9a101be58
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-01 09:18:23 +0900

    [MODIFY] <lib/api/Outing> 종료 시간 수정 API 추가

--- src/lib/api/Outing/index.ts
+++ src/lib/api/Outing/index.ts
@@ -1,8 +1,15 @@
 import { apiDefault } from "../client";
+import { ResDefault } from "../payloads";
 import { ResOutingCardListItem } from "../payloads/OutingCard";
 
 export const getEndOutings = () => {
   return apiDefault().get<{
     outings: ResOutingCardListItem[];
   }>(`/outings/with-filter?status=5`);
+};
+
+export const patchEndTime = (outingUuid: string, endTime: number) => {
+  return apiDefault().patch<ResDefault>(`/outings/uuid/${outingUuid}`, {
+    end_time: endTime
+  });
 };
