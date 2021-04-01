ff046df7dc233c180228e0734a8422bb4104141a
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-01 09:17:34 +0900

    [ADD] <lib/hooks/useEndTime> 종료 시간 훅 추가

---
+++ src/lib/hooks/useEndTime.ts
@@ -0,0 +1,74 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { toast } from "react-toastify";
+
+import { patchEndTime } from "../api/Outing";
+import { getAxiosError } from "../utils";
+
+const useEndTime = (
+  outingUuid: string,
+  start_time: number,
+  end_time: number,
+  endTime: string
+) => {
+  const history = useHistory();
+  const [endHour, setEndHour] = useState<string>("04");
+  const [endMin, setEndMin] = useState<string>("00");
+
+  const handleEndHour = (e: ChangeEvent<HTMLSelectElement>) => {
+    setEndHour(e.currentTarget.value);
+  };
+
+  const handleEndMin = (e: ChangeEvent<HTMLSelectElement>) => {
+    setEndMin(e.currentTarget.value);
+  };
+
+  const onClickChangeOutTime = async () => {
+    const end = new Date(end_time * 1000);
+    end.setHours(+endHour + 12, +endMin);
+
+    if (+end < +new Date()) {
+      toast.error("현재 시간보다 종료 시간이 늦을 수 없습니다.");
+      return;
+    }
+    if (+end < +new Date(start_time * 1000)) {
+      toast.error("종료 시간이 시작 시간보다 늦을 수 없습니다.");
+      return;
+    }
+    if (end.getHours() === 20 && end.getMinutes() > 30) {
+      toast.error("종료 시간은 최대 8시 30분까지 설정할 수 있습니다.");
+      return;
+    }
+
+    try {
+      await patchEndTime(outingUuid, end.getTime() / 1000);
+      toast.success("종료 시간을 변경했습니다.");
+    } catch (err) {
+      const { status } = getAxiosError(err);
+
+      if (status === 403) {
+        toast.error("선생님 계정으로 이용해주세요.");
+        history.push("/login");
+      } else if (status === 404) {
+        toast.error("존재하지 않는 외출증입니다.");
+      } else if (status === 409) {
+        toast.error("종료 시간을 수정할 수 없는 외출증입니다.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    setEndHour(endTime.split(":")[0]);
+    setEndMin(endTime.split(":")[1]);
+  }, [endTime]);
+
+  return [
+    endHour,
+    endMin,
+    handleEndHour,
+    handleEndMin,
+    onClickChangeOutTime
+  ] as const;
+};
+
+export default useEndTime;
