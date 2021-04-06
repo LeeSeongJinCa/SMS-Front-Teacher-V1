d566671e4ccc2429d5a514f1e799f00d8532bf09
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-06 15:39:11 +0900

    [MODIFY] <containers/Admin/Outing> 페이지 이동 시 외출증 배열 초기화

--- src/containers/Admin/Outing/certified/list/AdminOutingCertifiedListContainer.tsx
+++ src/containers/Admin/Outing/certified/list/AdminOutingCertifiedListContainer.tsx
@@ -1,19 +1,25 @@
-import React, { FC } from "react";
-import { useEffect } from "react";
+import React, { FC, useEffect } from "react";
 import { useDispatch } from "react-redux";
+
 import { AdminOutingCertifiedList } from "../../../../../components";
 import { OutingStatus } from "../../../../../lib/api/payloads/Outing";
-import { getOutingCardListSaga } from "../../../../../modules/action/outingCard";
+import {
+  getOutingCardListSaga,
+  setOutingCardList
+} from "../../../../../modules/action/outingCard";
 
 const AdminOutingCertifiedListContainer: FC = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
+    dispatch(setOutingCardList([]));
     dispatch(
       getOutingCardListSaga({
         status: OutingStatus["외출 인증 승인"]
       })
     );
   }, []);
+
   return <AdminOutingCertifiedList />;
 };
 
