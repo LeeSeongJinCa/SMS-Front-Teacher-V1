b085af87aa17ff1682fd80df05ba34f0405531bb
YeongG <yeonggil223@gmail.com>
2021-04-12 01:21:38 +0900

    [MODIFY] 타입 지정 안된거  수정

--- src/components/Admin/Outing/Done/AdminOutingDone.tsx
+++ src/components/Admin/Outing/Done/AdminOutingDone.tsx
@@ -9,7 +9,7 @@ import { OutingCardPage } from "../../../default";
 const AdminOutingCertifiedList: FC = () => {
   const dispatch = useDispatch();
   const clickHandler = useCallback(async (uuid: string) => {
-    const confirm = await Confirm.confirm([
+    const confirm: boolean = await Confirm.confirm([
       "외출이 종료된 학생의 외출증을 최종 확인하겠습니까?",
       "취소",
       "확인"
