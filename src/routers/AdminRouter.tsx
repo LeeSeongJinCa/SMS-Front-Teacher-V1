275b090aa9801cdb5b03b47e781fab36f4e9e616
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-20 16:56:13 +0900

    [MODIFY] <routers/AdminRouter> route lazy component

--- src/routers/AdminRouter.tsx
+++ src/routers/AdminRouter.tsx
@@ -1,23 +1,27 @@
 import React, { FC } from "react";
 import { Route, Switch, useHistory } from "react-router-dom";
 
-import { PageNotFound, AdminStatistics, MyPage, Login } from "../components";
 import {
-  AdminOutingCertifiedListContainer,
-  AdminOutingNowListContainer,
-  AdminOutingWaitListContainer,
-  AdminMainContainer,
-  AdminNoticeAllListContainer,
-  AdminNoticeAllDetailContainer,
-  AdminNoticeMineContainer,
-  AdminNoticeWritingContainer,
-  PasswordChangeContainer,
-  AdminOutingDoneContainer,
-  AdminNoticeMineDetailContainer,
-  AdminNoticeEditContainer,
-  AccountContainer,
-  AdminOutingApprovedListContainer
-} from "../containers";
+  LazyMain,
+  LazyPassword,
+  LazyAccount,
+  LazyOutingCertified,
+  LazyOutingNow,
+  LazyOutingWait,
+  LazyOutingDone,
+  LazyOutingApproved,
+  LazyOutingStatistics,
+  LazyNoticeAllList,
+  LazyNoticeAllDetail,
+  LazyNoticeMine,
+  LazyNoticeMineDetail,
+  LazyNoticeWriting,
+  LazyNoticeEdit,
+  LazyLogin,
+  LazyMyPage,
+  LazyPageNotFound
+} from "./AdminLazy";
+
 import { GlobalInnerBody } from "../GlobalStyle";
 
 const AdminRouter: FC = () => {
@@ -30,56 +34,24 @@ const AdminRouter: FC = () => {
       isBackNeed={needWhite.some(path => pathname.includes(path))}
     >
       <Switch>
-        <Route exact path="/" component={AdminMainContainer} />
-        <Route exact path="/pw-change" component={PasswordChangeContainer} />
-        <Route exact path="/account" component={AccountContainer} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/user" component={MyPage} />
-        <Route
-          exact
-          path="/out/wait"
-          component={AdminOutingWaitListContainer}
-        />
-        <Route exact path="/out/now" component={AdminOutingNowListContainer} />
-        <Route exact path="/out/done" component={AdminOutingDoneContainer} />
-        <Route
-          exact
-          path="/out/approved"
-          component={AdminOutingApprovedListContainer}
-        />
-        <Route
-          exact
-          path="/out/certified"
-          component={AdminOutingCertifiedListContainer}
-        />
-        <Route exact path="/out/statistics" component={AdminStatistics} />
-        <Route
-          exact
-          path="/notice/all"
-          component={AdminNoticeAllListContainer}
-        />
-        <Route
-          exact
-          path="/notice/all/:id"
-          component={AdminNoticeAllDetailContainer}
-        />
-        <Route exact path="/notice/mine" component={AdminNoticeMineContainer} />
-        <Route
-          exact
-          path="/notice/edit/:id"
-          component={AdminNoticeEditContainer}
-        />
-        <Route
-          exact
-          path="/notice/mine/:id"
-          component={AdminNoticeMineDetailContainer}
-        />
-        <Route
-          exact
-          path="/notice/writing"
-          component={AdminNoticeWritingContainer}
-        />
-        <Route path="*" component={PageNotFound} />
+        <Route exact path="/" component={LazyMain} />
+        <Route exact path="/pw-change" component={LazyPassword} />
+        <Route exact path="/account" component={LazyAccount} />
+        <Route exact path="/login" component={LazyLogin} />
+        <Route exact path="/user" component={LazyMyPage} />
+        <Route exact path="/out/wait" component={LazyOutingWait} />
+        <Route exact path="/out/now" component={LazyOutingNow} />
+        <Route exact path="/out/done" component={LazyOutingDone} />
+        <Route exact path="/out/approved" component={LazyOutingApproved} />
+        <Route exact path="/out/certified" component={LazyOutingCertified} />
+        <Route exact path="/out/statistics" component={LazyOutingStatistics} />
+        <Route exact path="/notice/all" component={LazyNoticeAllList} />
+        <Route exact path="/notice/all/:id" component={LazyNoticeAllDetail} />
+        <Route exact path="/notice/mine" component={LazyNoticeMine} />
+        <Route exact path="/notice/edit/:id" component={LazyNoticeEdit} />
+        <Route exact path="/notice/mine/:id" component={LazyNoticeMineDetail} />
+        <Route exact path="/notice/writing" component={LazyNoticeWriting} />
+        <Route path="*" component={LazyPageNotFound} />
       </Switch>
     </GlobalInnerBody>
   );
