9a907fe934df497153fb9754c89ce7b2a9a07dca
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-20 18:52:05 +0900

    [MODIFY] 성능 향상

--- src/routers/AdminLazy.ts
+++ src/routers/AdminLazy.ts
@@ -1,60 +1,112 @@
 import { lazy } from "react";
 
-export const LazyMyPage = lazy(() => import("../components/MyPage/MyPage"));
+export const LazyMyPage = lazy(
+  () => import(/* webpackChunkName: "my-page" */ "../components/MyPage/MyPage")
+);
 
-export const LazyLogin = lazy(() => import("../components/Login/Login"));
+export const LazyLogin = lazy(
+  () => import(/* webpackChunkName: "login" */ "../components/Login/Login")
+);
 
 export const LazyMain = lazy(
-  () => import("../containers/Admin/Main/AdminMainContainer")
+  () =>
+    import(
+      /* webpackChunkName: "out.wait" */ "../containers/Admin/Main/AdminMainContainer"
+    )
 );
 
 export const LazyPassword = lazy(
-  () => import("../containers/PasswordChange/PasswordChangeContainer")
+  () =>
+    import(
+      /* webpackChunkName: "password-change" */ "../containers/PasswordChange/PasswordChangeContainer"
+    )
 );
 
 export const LazyAccount = lazy(
-  () => import("../containers/Account/AccountContainer")
+  () =>
+    import(
+      /* webpackChunkName: "account" */ "../containers/Account/AccountContainer"
+    )
 );
 
 export const LazyPageNotFound = lazy(
-  () => import("../components/default/NotFound/NotFound")
+  () =>
+    import(
+      /* webpackChunkName: "page-not-found" */ "../components/default/NotFound/NotFound"
+    )
 );
 
 export const LazyOutingCertified = lazy(
-  () => import("../containers/Admin/Outing/AdminOutingCertifiedContainer")
+  () =>
+    import(
+      /* webpackChunkName: "out.certified" */ "../containers/Admin/Outing/AdminOutingCertifiedContainer"
+    )
 );
 
 export const LazyOutingNow = lazy(
-  () => import("../containers/Admin/Outing/AdminOutingNowListContainer")
+  () =>
+    import(
+      /* webpackChunkName: "out.now" */ "../containers/Admin/Outing/AdminOutingNowListContainer"
+    )
 );
 export const LazyOutingWait = lazy(
-  () => import("../containers/Admin/Outing/AdminOutingWaitListContainer")
+  () =>
+    import(
+      /* webpackChunkName: "out.wait" */ "../containers/Admin/Outing/AdminOutingWaitListContainer"
+    )
 );
 export const LazyOutingDone = lazy(
-  () => import("../containers/Admin/Outing/AdminOutingDoneConatiner")
+  () =>
+    import(
+      /* webpackChunkName: "out.done" */ "../containers/Admin/Outing/AdminOutingDoneConatiner"
+    )
 );
 export const LazyOutingApproved = lazy(
-  () => import("../containers/Admin/Outing/AdminOutingApprovedContainer")
+  () =>
+    import(
+      /* webpackChunkName: "out.approved" */ "../containers/Admin/Outing/AdminOutingApprovedContainer"
+    )
 );
 export const LazyOutingStatistics = lazy(
-  () => import("../components/Admin/Outing/statistics")
+  () =>
+    import(
+      /* webpackChunkName: "out.statistics" */ "../components/Admin/Outing/statistics"
+    )
 );
 
 export const LazyNoticeAllList = lazy(
-  () => import("../containers/Admin/Notice/all/AdminNoticeListContainer")
+  () =>
+    import(
+      /* webpackChunkName: "notice.all" */ "../containers/Admin/Notice/all/AdminNoticeListContainer"
+    )
 );
 export const LazyNoticeAllDetail = lazy(
-  () => import("../containers/Admin/Notice/all/AdminNoticeAllDetailContainer")
+  () =>
+    import(
+      /* webpackChunkName: "notice.all.detail" */ "../containers/Admin/Notice/all/AdminNoticeAllDetailContainer"
+    )
 );
 export const LazyNoticeMine = lazy(
-  () => import("../containers/Admin/Notice/mine/AdminNoticeMineContainer")
+  () =>
+    import(
+      /* webpackChunkName: "notice.mine" */ "../containers/Admin/Notice/mine/AdminNoticeMineContainer"
+    )
 );
 export const LazyNoticeMineDetail = lazy(
-  () => import("../containers/Admin/Notice/mine/AdminNoticeMineDetailContainer")
+  () =>
+    import(
+      /* webpackChunkName: "notice.mine.detail" */ "../containers/Admin/Notice/mine/AdminNoticeMineDetailContainer"
+    )
 );
 export const LazyNoticeWriting = lazy(
-  () => import("../containers/Admin/Notice/writing/AdminNoticeWritingContainer")
+  () =>
+    import(
+      /* webpackChunkName: "notice.writing" */ "../containers/Admin/Notice/writing/AdminNoticeWritingContainer"
+    )
 );
 export const LazyNoticeEdit = lazy(
-  () => import("../containers/Admin/Notice/edit/AdminNoticeEditContainer")
+  () =>
+    import(
+      /* webpackChunkName: "notice.edit" */ "../containers/Admin/Notice/edit/AdminNoticeEditContainer"
+    )
 );
